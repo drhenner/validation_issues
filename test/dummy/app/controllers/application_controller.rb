2d7477f39cef0def8efe9cb105d153152ecd01fd
David Henner <drhenner@yahoo.com>
2012-07-24 01:18:39 -0400

    add has_validation_issues_admin_privilages to dummy app in tests

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  protected
+  def has_validation_issues_admin_privileges
+    # logic you want to make this true
+    # Typically this will work
+    # current_user && current_user.admin?
+    true
+  end
+
 end
