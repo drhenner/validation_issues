405353ad1a093dea2dd10d41a62420b2fc8b1187
David Henner <drhenner@yahoo.com>
2012-07-24 02:58:45 -0400

    get SQL to work on postgres

--- app/models/validation_issue.rb
+++ app/models/validation_issue.rb
@@ -27,8 +27,8 @@ class ValidationIssue < ActiveRecord::Base
 
     def increment_with_sql!(attribute, by = 1)
       raise ArgumentError("Invalid attribute: #{attribute}") unless attribute_names.include?(attribute.to_s)
-      original_value_sql = "CASE WHEN `#{attribute}` IS NULL THEN 0 ELSE `#{attribute}` END"
-      self.class.update_all("`#{attribute}` = #{original_value_sql} + #{by.to_i}", "id = #{id}")
+      #original_value_sql = "CASE WHEN `#{attribute}` IS NULL THEN 0 ELSE `#{attribute}` END"
+      self.class.update_all("#{attribute} = #{attribute} + #{by.to_i}", "id = #{id}")
       reload
     end
 end
