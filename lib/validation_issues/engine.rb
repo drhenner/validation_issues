b80e8e32244972f8d39f938b6dcf3a31d4066128
David Henner <drhenner@yahoo.com>
2012-07-24 20:05:21 -0400

    precompile failing

--- lib/validation_issues/engine.rb
+++ lib/validation_issues/engine.rb
@@ -1,4 +1,27 @@
 module ValidationIssues
   class Engine < ::Rails::Engine
+      # Config defaults
+      config.mount_at = '/'
+
+      # Enabling assets precompiling under rails 3.1
+      if Rails.version >= '3.1'
+        initializer :assets do |config|
+          Rails.application.config.assets.precompile += %w( validation_issues/validation_issues.js validation_issues/validation_issues.css )
+        end
+      end
+
+      # Check the gem config
+      initializer "check config" do |app|
+        # make sure mount_at ends with trailing slash
+        config.mount_at += '/'  unless config.mount_at.last == '/'
+      end
+
+      initializer :flash_cookie do |config|
+        config.middleware.insert_after(
+          'ActionDispatch::Cookies',
+          ValidationIssues::Middleware::FlashSessionCookie,
+          ::Rails.configuration.session_options[:key]
+        )
+      end
   end
 end
