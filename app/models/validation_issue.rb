2b601f4f3488ed920d218f9c5dabc40de7cdbf3f
drhenner <drhenner@yahoo.com>
2015-07-17 17:05:32 -0700

    update for rails 4 deprecations

--- app/models/validation_issue.rb
+++ app/models/validation_issue.rb
@@ -28,7 +28,8 @@ class ValidationIssue < ActiveRecord::Base
     def increment_with_sql!(attribute, by = 1)
       raise ArgumentError("Invalid attribute: #{attribute}") unless attribute_names.include?(attribute.to_s)
       #original_value_sql = "CASE WHEN `#{attribute}` IS NULL THEN 0 ELSE `#{attribute}` END"
-      self.class.update_all("#{attribute} = #{attribute} + #{by.to_i}", "id = #{id}")
+      self.class.where("id = #{id}").update_all("#{attribute} = #{attribute} + #{by.to_i}")
+
       reload
     end
 end
--- lib/validation_issues/version.rb
+++ lib/validation_issues/version.rb
@@ -1,3 +1,3 @@
 module ValidationIssues
-  VERSION = "0.2.2"
+  VERSION = "0.2.3"
 end
